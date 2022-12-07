86319b3ff93e3d38eb24134920a11c7d28d17e5f
Сергей Катаев <nimnul010@yandex.ru>
2022-12-07 21:47:25 +0300

    pytest PASSED

---
+++ yatube/posts/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Post
+from .models import Group
+ 
+ 
+def index(request):
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': title
+    }
+    return render(request, 'posts/index.html', context)
+ 
+ 
+def group_posts(request, slug):
+    title = f'Записи сообщетсва {Group.objects.filter(slug=slug)[0]}'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title
+    }
+    return render(request, 'posts/group_list.html', context)
